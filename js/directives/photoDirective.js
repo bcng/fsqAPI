f8e4bfc5162c40cbf38d5ec05171f499111deb38
Brian Ng <bcn220@gmail.com>
2015-08-05 14:40:00 -0600

    add angular ui directive, implement tooltip on photos

--- js/directives/photoDirective.js
+++ js/directives/photoDirective.js
@@ -6,15 +6,16 @@
              photo: '=',
          },
          link: function(scope, element, attributes) {
-             element.on('mouseenter', function() {
-                 element.css({
-                     'opacity': '0.3'
-                 }).on('mouseleave', function(){
-                     element.css({
-                         'opacity': '1.0'
-                     })
-                 })
-             })
+             // element.on('mouseenter', function() {
+             //     element.css({
+             //         'opacity': '0.3'
+             //     }).on('mouseleave', function(){
+             //         element.css({
+             //             'opacity': '1.0'
+             //         })
+             //     })
+             // })
+            
              
          }
      }
--- js/services/photoService.js
+++ js/services/photoService.js
@@ -5,6 +5,7 @@ app.service('photoService', function($http, $q) {
         var deferred = $q.defer();
         $http.get('https://api.foursquare.com/v2/users/self/photos?oauth_token=PZCHCVVFF43VW3N25XQELTYOFJYVTFDK1OVD0CMQ15CXZKWT&v=20150801').then(function(response) {
             var parsedResponse = response.data.response.photos;
+            console.log(parsedResponse);
             deferred.resolve(parsedResponse)
         })
         return deferred.promise;
